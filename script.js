f41ed2b1fa7807a32496820442e0d2285d124392
garoloup <fredwolff@free.fr>
2021-01-23 18:37:53 +0100

    Variables, integer, float & string

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("coucou 3");
+console.log("script start");
+
+var name = "Fred";
+// display in back
+console.log(name);
+console.log(name.length);
+
+var nb = 12;
+// display in green
+console.log(nb);
+
+var nb2 = 12.55;
+// display in green
+console.log(nb2);
+// display in black
+console.log(nb2.toString());
+
+var xstr = "33.88";
+// convert in interger
+console.log(parseInt(xstr));
+// convertin float
+console.log(parseFloat(xstr));
+
+var pos = name.indexOf("r");
+console.log(pos);
